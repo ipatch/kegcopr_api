98c866fd7627c2d2c52a07d86f8145b6bac0076a
ipatch <chris.r.jones.1983@gmail.com>
2017-05-02 21:24:01 -0500

    update accounts/user.ex `model`

--- lib/kegcopr_api/accounts/user.ex
+++ lib/kegcopr_api/accounts/user.ex
@@ -8,4 +8,18 @@ defmodule KegCopRAPI.Accounts.User do
 
     timestamps()
   end
+
+  @required_fields ~w(email encrypted_password username)
+  @optional_fields ~w()
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:username, min: 1, max: 20)
+    |> update_change(:email, &String.downcase/1)
+    |> unique_constraint(:email)
+    |> update_change(:username, &String.downcase/1)
+    |> unique_constraint(:username)
+  end
 end
