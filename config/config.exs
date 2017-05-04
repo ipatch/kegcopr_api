1e25f4ee956734b61bcd5d729a22a7f3ba91c419
ipatch <chris.r.jones.1983@gmail.com>
2017-05-03 23:16:52 -0500

    add configuration block to `config.exs` file for configuring guardian

--- config/config.exs
+++ config/config.exs
@@ -23,6 +23,13 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# configure guardian
+config :guardian, Guardian,
+  issuer: "KegCopRAPI",
+  ttl: {30, :days},
+  verify_issuer: true,
+  serializer: KegCopRAPI.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
