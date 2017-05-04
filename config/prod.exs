9dd09f8dc1dbf4bb1e7526d7a830a34222f45c04
ipatch <chris.r.jones.1983@gmail.com>
2017-05-03 23:30:28 -0500

    add guardian secret_key conf to prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -81,6 +81,9 @@ config :kegcopr_api, KegCopRAPI.Repo,
 #
 config :kegcopr_api, KegCopRAPI.Web.Endpoint, server: true
 #
+config :guardian, Guardian,
+  secret_key: System.get_env("GUARDIAN_SECRET_KEY")
+
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
