deca92c1acee1fcad633e4f616b9f9938e783c7b
ipatch <chris.r.jones.1983@gmail.com>
2017-05-05 12:13:02 -0500

    modify the `user_controller.ex` file.

--- lib/kegcopr_api/web/controllers/user_controller.ex
+++ lib/kegcopr_api/web/controllers/user_controller.ex
@@ -17,10 +17,15 @@ defmodule KegCopRAPI.Web.UserController do
 
     case Repo.insert(changeset) do
       {:ok, user} ->
-        conn
+        new_conn = Guardian.Plug.api_sign_in(conn, user, :access)
+        jwt = Guardian.Plug.current_token(new_conn)
+        # conn
+        # |> put_status(:created)
+        # |> put_resp_header("location", user_path(conn, :show, user))
+        # |> render("show.json", user: user)
+        new_conn
         |> put_status(:created)
-        |> put_resp_header("location", user_path(conn, :show, user))
-        |> render("show.json", user: user)
+        |> render(KegCopRAPI.SessionView, "show.json", user: user, jwt: jwt)
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
