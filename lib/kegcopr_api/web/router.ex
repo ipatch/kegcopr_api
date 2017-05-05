902dcd339a1b4a57f2cca28e71f16fb1bd0f081d
ipatch <chris.r.jones.1983@gmail.com>
2017-05-05 12:02:58 -0500

    uncomment plug Guardian from the `router.ex` file

--- lib/kegcopr_api/web/router.ex
+++ lib/kegcopr_api/web/router.ex
@@ -11,7 +11,7 @@ defmodule KegCopRAPI.Web.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
-    # plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
     # plug Guardian.Plug.LoadResource
   end
 
