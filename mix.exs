13376a22db8e05a90a2d5ceb419c3cb32caca495
ipatch <chris.r.jones.1983@gmail.com>
2017-05-10 23:26:25 -0500

    bump build to 0.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule KegCopRAPI.Mixfile do
 
   def project do
     [app: :kegcopr_api,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
